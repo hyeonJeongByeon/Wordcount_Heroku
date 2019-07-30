77cea656efe4ea5f3f7607fa68d9c2ead7f1879d
hyeonJeongByeon <jungjung1218@gmail.com>
2019-07-30 19:44:33 -0400

    comment

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/byeonhyeonjeong/Documents/LikeLion/wordcountsession-master 2/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
